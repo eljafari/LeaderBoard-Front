3961073b0c5b17627957c3ccc317776955492056
elham jafari <elhamjafari@MacBook-Pro.local>
2023-11-19 15:15:33 -0800

    add search box and sorting by name/points snd some style adjustment

--- src/UserList.js
+++ src/UserList.js
@@ -5,6 +5,11 @@ import './UserList.css';
 
 function UserList() {
   const [users, setUsers] = useState([]);
+  const [selectedUser, setSelectedUser] = useState(null);
+  const [search, setSearch] = useState('');
+  const [sortBy, setSortBy] = useState(null);
+  const [sortOrder, setSortOrder] = useState('asc'); 
+
 
   useEffect(() => {
      // Fetch users from the API endpoint
@@ -16,6 +21,36 @@ function UserList() {
         console.error('Error fetching data:', error);
       });
   }, []);
+
+  const handleUserClick = (user) => {
+    setSelectedUser(user);
+  };  
+
+  const handleSort = (field) => {
+    if (sortBy === field) {
+      // If the same field is clicked again, reverse the sorting order
+      setSortOrder(sortOrder === 'asc' ? 'desc' : 'asc');
+    } else {
+      // If a different field is clicked, set it as the new sorting field
+      setSortBy(field);
+      setSortOrder('asc'); // Default to ascending order for the new field
+    }
+  };
+  
+  const filteredUsers = users
+    .filter((user) => user.name.toLowerCase().includes(search.toLowerCase()))
+    .sort((a, b) => {
+      if (sortBy === 'name') {
+        return sortOrder === 'asc'
+          ? a.name.localeCompare(b.name)
+          : b.name.localeCompare(a.name);
+      } else if (sortBy === 'points') {
+        return sortOrder === 'asc'
+          ? a.points - b.points
+          : b.points - a.points;
+      }
+      return 0;
+    });
 
   // Make a PUT request to update user points
   const updateUserPoints = (user, change) => {
@@ -49,23 +84,56 @@ function UserList() {
       });
   };
 
-
-
   return (
     <div>
       <h2>Leaderboard Application</h2>
-      <ul>
-        {users.map((user) => (
-          <li key={user.id}>
+      <input className='searchBox'
+        type="text"
+        placeholder="Search by name"
+        value={search}
+        onChange={(e) => setSearch(e.target.value)}
+      />
+      <table>
+        <thead>
+          <tr className='sorting'>
+            <th onClick={() => handleSort('name')}>
+              Sort by Name
+              {sortBy === 'name' && (
+                <span className={`arrow ${sortOrder}`}></span>
+              )}
+            </th>
+            <th onClick={() => handleSort('points')}>
+            Sort by Points
+              {sortBy === 'points' && (
+                <span className={`arrow ${sortOrder}`}></span>
+              )}
+            </th>
+          </tr>
+        </thead>
+        <tbody>
+        <ul>
+          {filteredUsers.map((user) => (
+            <li key={user.id} onClick={() => handleUserClick(user)}>
             <button className='button delete' onClick={() => deleteUser(user.id)}>Delete</button>
-            <span>Name: {user.name}</span>
+            <span  onClick={() => handleUserClick(user)}>Name: {user.name}</span>
             <span>Points: {user.points}</span>
             <button className='button' onClick={() => updateUserPoints(user, 1)}>+</button>
             <button className='button'  onClick={() => updateUserPoints(user, -1)}>-</button>
           </li>
-        ))}
-      </ul>
+          ))}
+        </ul>
+        </tbody>
+      </table>
       <Link className='addButton' to="/create-user">Add User</Link>
+      {selectedUser && (
+        <div className="user-details">
+          <h3>User Details</h3>
+          <p>Name: {selectedUser.name}</p>
+          <p>Age: {selectedUser.age}</p>
+          <p>Points: {selectedUser.points}</p>
+          <p>Address: {selectedUser.address}</p>
+        </div>
+      )}
     </div>
   );
 }
